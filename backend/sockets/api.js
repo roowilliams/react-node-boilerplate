aea64d5ef7cdfe23ddf8d6046fd3ad836a3c8d9f
Roo Williams <roow@twitter.com>
2019-04-06 10:14:43 -0400

    update message

--- backend/sockets/api.js
+++ backend/sockets/api.js
@@ -1,23 +1,23 @@
-var socketio = require('socket.io');
-var io = socketio();
+var socketio = require('socket.io')
+var io = socketio()
 
 class socketApi {
   constructor() {
-    this.io = io;
-    this.initListeners();
+    this.io = io
+    this.initListeners()
   }
 
   initListeners() {
     this.io.on('connection', socket => {
-      console.log('A user connected');
-      this.sendEvent('message', { msg: 'hello you' });
-    });
+      console.log('A user connected')
+      this.sendEvent('message', { msg: 'hello' })
+    })
   }
 
   sendEvent(type, data) {
-    this.io.sockets.emit(type, data);
+    this.io.sockets.emit(type, data)
   }
 }
 
-const instance = new socketApi();
-module.exports = instance;
+const instance = new socketApi()
+module.exports = instance
