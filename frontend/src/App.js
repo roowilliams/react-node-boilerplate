e5de469f864e99eacf69528c2ce3aba7e380abfb
Roo Williams <roow@twitter.com>
2019-04-06 10:13:51 -0400

    load socket connection address from package.json proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,36 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import openSocket from "socket.io-client";
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import packageJson from '../package.json'
+import openSocket from 'socket.io-client'
 
 class App extends Component {
   constructor() {
-    super();
-    const socket = openSocket("http://localhost:3001");
-    socket.on("message", message => console.log(message));
+    super()
+    const socket = openSocket(packageJson.proxy)
+    socket.on('message', message => console.log(message))
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
